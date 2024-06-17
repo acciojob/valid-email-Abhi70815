79e7cb007ae312a8bacc2c0f8f6a16dfa4795285
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-17 14:24:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function validEmail(str) {
   //your JS code here.
+	  // Define the regular expression pattern for a valid email address
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,3}$/;
+  // Test the input string against the pattern
+  return emailPattern.test(str);
 }
 
 // Do not change the code below.
